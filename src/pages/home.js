54a9798f68514f737d3fa277012edee75c820fa7
Ruben Chirino <RubenChirino@github.com>
2020-06-17 18:49:18 -0300

    SideBar added v3 (dev)

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Fragment } from "react";
 import { useLocation } from "wouter";
 import usePictures from "../hooks/usePictures";
 import ListOfPictures from "../components/ListOfPictures";
+import avatarImg from "../images/avatar.png";
+import '../styles/sidebar.css';
 
 export default function Home( { params } ){
 
@@ -15,15 +17,63 @@ export default function Home( { params } ){
 
     const {loading, pictures} = usePictures();
      
-    return( <div className="container">
+    function handleMenu(){
+
+        const menu = document.querySelector('#sidemenu');
+
+        menu.classList.toggle("menu-expanded");
+        menu.classList.toggle("menu-collapsed");
+
+        document.querySelector('body').classList.toggle('body-expanded');
+    }
+
+    return( <Fragment>
+         
+    <div id="sidemenu" className="menu-collapsed">
+        { /* Header */ }
+        <div id="header">
+            <div id="title"><span>Vida MRR</span></div>
+
+            <div id="menu-btn" onClick={handleMenu}>
+               <div className="btn-hamburguer"></div>
+               <div className="btn-hamburguer"></div>
+               <div className="btn-hamburguer"></div>
+            </div>
+
+        </div>
+        { /* Profile */ }
+        <div id="profile">
+            <div id="photo"><img src={avatarImg} alt="" /></div>
+
+            <div id="name"><span>Marco Polo</span></div>
+        </div>
+        { /* Items */ }
+        <div id="menu-items">
+
+            <div className="item" id="photo">
+                <a href="#">
+                    <div className="icon"><img src={avatarImg} alt="" /></div>
+                    <div className="title"><span>Cloud Services</span></div>
+                </a>
+            </div>
+
+                
+
+        </div>
+
+    </div>
+
+    <div id="main-container" className="container"> { /* container */ }
+
+        <h1>Hola a todos!</h1>
 
         <div className="row mt-4">
             <ListOfPictures pictures={pictures} />
         </div>
     
-
     </div>
-        
+
+    </Fragment> 
     );
 
 }
